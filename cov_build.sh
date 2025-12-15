0e9449519060050ca0d78202813ba1cc9019d0b9
Sathishkumar Deena Kirupakaran <Sathishkumar_deenakirupakaran@comcast.com>
2025-12-15 14:29:27 -0500

    RDKEMW-11604: CI rename entservices-infra to appgateway

--- cov_build.sh
+++ cov_build.sh
@@ -5,11 +5,11 @@ set -e
 GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 ############################
-# Build entservices-infra
-echo "buliding entservices-infra"
+# Build entservices-appgateway
+echo "building entservices-appgateway"
 
 cd ${GITHUB_WORKSPACE}
-cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-infra \
+cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-appgateway \
 -DUSE_THUNDER_R4=ON \
 -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
 -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
