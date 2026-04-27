bbbc747f9ca9e3de64b6b802296be0734e6ac75d
balav08 <54432605+balav08@users.noreply.github.com>
2026-04-27 12:27:10 +0530

    Create SystemServicesMock.h

---
+++ Tests/mocks/SystemServicesMock.h
@@ -0,0 +1,46 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2026 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <gmock/gmock.h>
+#include <interfaces/ISystemServices.h>
+
+using ::WPEFramework::Exchange::ISystemServices;
+
+class SystemServicesMock : public ISystemServices {
+public:
+    SystemServicesMock() = default;
+    virtual ~SystemServicesMock() = default;
+
+    MOCK_METHOD(WPEFramework::Core::hresult, Register, (INotification* notification), (override));
+    MOCK_METHOD(WPEFramework::Core::hresult, Unregister, (INotification* notification), (override));
+    MOCK_METHOD(WPEFramework::Core::hresult, GetDeviceInfo, (const string* selector, DeviceInfo& info), (const, override));
+    MOCK_METHOD(WPEFramework::Core::hresult, GetFriendlyName, (string& name, bool& success), (const, override));
+    MOCK_METHOD(WPEFramework::Core::hresult, SetFriendlyName, (const string& name, SystemResult& result), (override));
+    MOCK_METHOD(WPEFramework::Core::hresult, GetSystemVersions, (SystemVersionsInfo& info), (const, override));
+    MOCK_METHOD(WPEFramework::Core::hresult, GetTerritory, (string& territory, string& region, bool& success), (const, override));
+    MOCK_METHOD(WPEFramework::Core::hresult, SetTerritory, (const string& territory, const string& region, SystemError& error, bool& success), (override));
+    MOCK_METHOD(WPEFramework::Core::hresult, GetTimeZoneDST, (string& timeZone, string& accuracy, bool& success), (const, override));
+    MOCK_METHOD(WPEFramework::Core::hresult, SetTimeZoneDST, (const string& timeZone, const string& accuracy, uint32_t& serviceStatus, string& errorMessage, bool& success), (override));
+
+    MOCK_METHOD(void, AddRef, (), (const, override));
+    MOCK_METHOD(uint32_t, Release, (), (const, override));
+    MOCK_METHOD(void*, QueryInterface, (const uint32_t interfaceNumber), (override));
+};
