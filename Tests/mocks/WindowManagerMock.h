205a2603708d812128f7cfa41763f46d0a596eb7
AdityaKasar <adityakasar1994@gmail.com>
2026-07-24 12:49:14 +0530

    NO_JIRA: fix window manager mock

--- Tests/mocks/WindowManagerMock.h
+++ Tests/mocks/WindowManagerMock.h
@@ -55,7 +55,7 @@ public:
         const uint32_t ownerId,
         const uint32_t groupId,
         const bool topmost,
-        const bool focus),
+        const bool focus,
         const string& capabilities));
     MOCK_METHOD(WPEFramework::Core::hresult, GetApps, (string& appsIds), (const, override));
     MOCK_METHOD(WPEFramework::Core::hresult, SetFocus, (const string& client), (override));
